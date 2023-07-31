8248788db425ce7f6b5246ba9fd7d6ead8fbbb8e
Daniel de Andrade <106408440+Daniel1702@users.noreply.github.com>
2023-07-31 20:55:52 -0300

    Add files via upload

--- Avaliativo Final- Interfaces Web II/seu_arquivo_js.js
+++ Avaliativo Final- Interfaces Web II/seu_arquivo_js.js
@@ -1,15 +1,20 @@
+// Variáveis para elementos do DOM
 const checkButton = document.getElementById('checkButton');
 const resetButton = document.getElementById('resetButton');
 const resultMessage = document.getElementById('resultMessage');
 const meuCabecalho = document.querySelector('#nome');
+const gifContainer = document.getElementById('gifContainer'); // Nova div para conter os GIFs
 
+// Função para limpar o formulário e o GIF container
 function resetForm() {
     document.getElementById('dayInput').value = '';
     document.getElementById('monthInput').value = '';
     document.getElementById('yearInput').value = '';
     resultMessage.textContent = '';
+    gifContainer.innerHTML = ''; // Limpa o conteúdo do gifContainer
 }
 
+// Função para calcular a idade e exibir o GIF correspondente
 function calculateAge() {
     const day = parseInt(document.getElementById('dayInput').value);
     const month = parseInt(document.getElementById('monthInput').value);
@@ -29,6 +34,13 @@ function calculateAge() {
         meuCabecalho.textContent = ' ';
     }
 
+    // Verifica se é maior ou igual a 18 anos e adiciona o GIF correspondente
+    if (ageInYears >= 18) {
+        gifContainer.innerHTML = '<iframe src="https://giphy.com/embed/rEIkMTmkU7tpNpFxbe" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/SpiderVerseMovie-spider-man-spiderverse-movie-spicer-man-across-the-spider-verse-rEIkMTmkU7tpNpFxbe">via GIPHY</a></p>';
+    } else {
+        gifContainer.innerHTML = '<iframe src="https://giphy.com/embed/n6Ux5rKzeXZsObXR8i" width="100%" height="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/sony-pictures-de-spiderman-milesmorales-spiderversefilm-n6Ux5rKzeXZsObXR8i">via GIPHY</a></p>';
+    }
+
     if (isNaN(ageInYears)) {
         resultMessage.textContent = 'Por favor, insira uma data válida.';
     } else if (ageInYears >= 18) {
@@ -38,6 +50,7 @@ function calculateAge() {
     }
 }
 
+// Event listeners
 checkButton.addEventListener('click', calculateAge);
 resetButton.addEventListener('click', resetForm);
 
@@ -46,8 +59,3 @@ document.addEventListener('keydown', function (event) {
         calculateAge();
     }
 });
-
-
-
-
-
